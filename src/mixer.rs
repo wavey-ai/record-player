cb2b0dd725214143482c97b1d79252cc5016b675
Jamie B <jamie1612@gmail.com>
2026-07-20 04:11:28 +0100

    Apply canonical Rust formatting

--- src/mixer.rs
+++ src/mixer.rs
@@ -1,5 +1,8 @@
 pub fn sharp_crossfader_gains(value: f32, width: f32) -> (f32, f32) {
     let x = value.clamp(0.0, 1.0);
     let width = width.clamp(0.0001, 1.0);
-    ((x / width).clamp(0.0, 1.0), ((1.0 - x) / width).clamp(0.0, 1.0))
+    (
+        (x / width).clamp(0.0, 1.0),
+        ((1.0 - x) / width).clamp(0.0, 1.0),
+    )
 }
